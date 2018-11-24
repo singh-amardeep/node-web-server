c9916ca6307e3766ed1679031af3beed1468b38e
amardeep <amardeep.singh@highpeaksw.com>
2018-11-24 09:06:10 +0530

    add project page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get("/bad", (req, res) => {
   });
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects"
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up in port ${PORT}`);
 });
